575cfe9a50a0b42412fe7ebfd71a9acaa2b6f071
arcorona23 <andrew.r.corona@vanderbilt.edu>
2024-09-17 16:32:08 -0500

    Create python_excercises.py

---
+++ python_excercises.py
@@ -0,0 +1,23 @@
+
+
+
+a = 7
+b = 3
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+if a > b:
+    print("a is greater than b")
+else:
+    print("a is not greater than b")
+
+
+
+
+people = {"James": 15, "Jake": 20, "John": 25}
+for name, age in people.items():
+    print(name, "is", age, "years old")
+
+people["Joe"] = 100
+print(people)
